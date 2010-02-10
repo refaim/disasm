dd95afd2cc955fb954ba6f2b23ee2c7904c2fe60
Roman Kharitonov <refaim.vl@gmail.com>
2010-02-10 18:23:36 +1000

    -

--- parse.asm
+++ parse.asm
@@ -1,21 +1,22 @@
 .model small
 locals
 
-public parse
 extrn print: far
 
+public parse
+
 data segment para public 'data' use16
-    a db "ONOTOLE!11$"
+    test_msg db "Hello World!$"
 data ends
 
 code segment para public 'code' use16
-assume cs:code, ds:data
+assume cs: code
 
 parse proc far
-    mov dx, offset a
+    lea dx, test_msg
     call print
     ret
 parse endp
-    
+
 code ends
 end
