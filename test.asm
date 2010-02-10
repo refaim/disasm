8a640e97118daebcf8b412afa3e3dd25b3f262ad
Roman Kharitonov <refaim.vl@gmail.com>
2010-02-10 18:22:08 +1000

    Tabs -> spaces

--- test.asm
+++ test.asm
@@ -1,14 +1,14 @@
 .model tiny   
 
 .data
-	db 0DEh,0ADh,0BEh,0EFh
+    db 0DEh,0ADh,0BEh,0EFh
 num equ 111111111111111b ; 42975
 
 .code
     org 100h
 start:
-	jne exit
-	mov ax, num
+    jne exit
+    mov ax, num
     and ax, ax
     jz exit
 
@@ -16,17 +16,17 @@ start:
     xor bx, bx
     xor dx, dx
     cycle:
-		shl ax, 1
-		jc found
-		xor bx, bx
-		jmp next
-		found:
-			inc bx
-			cmp bx, 4
-			jl next
-			inc dx
-		next:
+        shl ax, 1
+        jc found
+        xor bx, bx
+        jmp next
+        found:
+            inc bx
+            cmp bx, 4
+            jl next
+            inc dx
+        next:
     loop cycle
 exit:
-	ret
+    ret
 end start
