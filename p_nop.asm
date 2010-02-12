6dbcb63355a765e4a344d6ec59f9123895c3d3db
Roman Kharitonov <refaim.vl@gmail.com>
2010-02-12 16:16:31 +1000

    Use memcpy

--- p_nop.asm
+++ p_nop.asm
@@ -1,21 +1,31 @@
 .model small
 .386
 
-public parse_nop 
+extrn memcpy: far
+
+public parse_nop
+
+data segment para public 'data' use16
+    nop_msg db 'nop', 10
+data ends
 
 code segment para public 'code' use16
-assume cs:code
+assume cs: code
+
 parse_nop proc pascal far
-	cmp byte ptr [si], 90h
-	jne short @@exit
-        mov byte ptr [di], 'n'
-        mov byte ptr [di + 1], 'o'
-        mov byte ptr [di + 2], 'p'
-        mov byte ptr [di + 3], 10
-	inc si
-	add di, 4
+uses cx
+    cmp byte ptr [si], 90h
+    jne short @@exit
+    push si
+    lea si, nop_msg
+    mov cx, 4
+    call memcpy
+    pop si
+    inc si
+    add di, 4
 @@exit:
-	ret
+    ret
 parse_nop endp
+
 code ends
 end
