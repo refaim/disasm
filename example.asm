5f3efab5f490aa3bdd03c5afc075bf972e27ab33
Roman Kharitonov <refaim.vl@gmail.com>
2010-02-12 18:35:51 +1000

    FAQ & example added

---
+++ example.asm
@@ -0,0 +1,26 @@
+.model small
+.386
+locals
+
+extrn memcpy: far
+
+public parse
+
+data segment para public 'data' use16
+    ; your data
+data ends
+
+code segment para public 'code' use16
+assume cs: code
+
+parse proc pascal far
+;uses ax, bx, cx
+
+    ; your code
+
+@@exit:
+    ret
+parse_nop endp
+
+code ends
+end
