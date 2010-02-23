796ec5861eaecf95d7d66458c531c9285de22ab8
kravitz <idkravitz@gmail.com>
2010-02-23 17:14:16 +1000

    merged with changes, that refaim made

--- test.asm
+++ test.asm
@@ -1,76 +1,26 @@
 .model tiny
 .386
 
-generate_void macro
-    rept 500
-        nop
-    endm
-endm
-
-.DATA
-    w1  dw  1000
-    b1  db  19
-    db 0DEh,0ADh,0BEh,0EFh
-
 .code
     org 100h
 start:
-    test    word ptr [bp+di]+0AAh,0ABCDh
-
-    and al,1
-    and ch,255
-    or  dl,55
-    xor dh,32
-    test    bl,58
-    test    cl,0
-    or  bh,2
-    xor ah,254
-
-    nop
-    nop
-    nop 
-
-    and ax,1000h
-    or  cx,2000h
-    test    dx,1111h
-    xor bx,5678h
-    and sp,0EFABh
-    or  bp,0A000h
-    xor si,0BBBBh
-    test    di,0DDCh
-
-
-    nop
-    nop
-    nop
-
-
-    and byte ptr [bx+si],3h
-    or  byte ptr [bx+di],5h
-    xor byte ptr [bp+si],7h
-    test    byte ptr [bp+di],9h
-    and byte ptr [si],0Ah
-    or  byte ptr [di],0Bh
-    xor byte ptr ds:0050h,0Ch
-    test    byte ptr [bx],0Dh   
-
-    nop
-    nop
-    nop
+    sal ax, 1
+    sal bx, 2
+    sal cx, 3
+    sal dx, 4
+    sal sp, 5
+    sal bp, 6
+    sal si, 7
+    sal di, cl
     
-    and word ptr [bx+si]+09h,333h 
-    or  word ptr [bx+di]+78h,531h
-    xor word ptr [bp+si]+91h,312h
-    test    word ptr [bp+di]+0AAh,0ABCDh
-    and word ptr [si]+0FFh,0EEFh
-    xor word ptr [di]+0EDh,0ABCh
-    test    word ptr [bp]+0BDh,0BCAFh
-    or  word ptr [bx]+0AFh,073Ah
-
-    nop
-    nop
-    nop
-
+    sal al, 1   
+    sal cl, 2
+    sal dl, 3
+    sal bl, 4
+    sal ah, 5
+    sal ch, 6
+    sal dh, 7
+    sal bh, cl
     
     test    word ptr [bx+si]+0991h,33h 
     or  word ptr [bx+di]+7813h,53h
@@ -226,6 +176,33 @@ start:
     nop
     nop
 
+    sar al, cl   
+    sar cl, 7
+    sar dl, 6
+    sar bl, 5
+    sar ah, 4
+    sar ch, 3
+    sar dh, 2
+    sar bh, 1
+
+    sar ax, cl
+    sar bx, 7
+    sar cx, 6
+    sar dx, 5
+    sar sp, 4
+    sar bp, 3
+    sar si, 2
+    sar di, 1
+
+    sal byte ptr [bp+di+8], 4
+    sal byte ptr [ds:1000], cl
+    sal word ptr [ds:1000], cl
+    sar byte ptr [ds:8], cl
+    sar word ptr [ds:10], cl
+    sal ax, 1
+    sal bh, cl
+    sal ax, 3
+    sar byte ptr [si+300], 4
 exit:
     ret
 end start
